92cae75ce6014eaa0bd492c48e08ce0df2605cf3
Simon Bishop <simon@tenacious.digital>
2020-02-18 13:41:25 +0200

    test scripts, broker updates

---
+++ test/stress/broker.js
@@ -0,0 +1,41 @@
+const HappnerCluster = require("../..");
+const baseConfig = require("../_lib/base-config");
+const libDir = require("../_lib/lib-dir");
+const commander = require('commander');
+
+commander
+.option('--seq [number]', 'sequence number')
+.option('--min [number]', 'minimum peers')
+.option('--seed [number]', 'minimum peers')
+.option('--hosts [number]', 'hosts')
+.parse(process.argv);
+
+commander.seq = parseInt(commander.seq || 1);
+commander.hosts = commander.hosts || "127.0.0.1:56001,127.0.0.1:56002";
+
+function brokerInstanceConfig(seq, sync) {
+  var config = baseConfig(seq, sync, true, null, null, commander.hosts);
+  config.authorityDelegationOn = true;
+  config.modules = {
+    localComponent: {
+      path: libDir + "integration-09-local-component"
+    },
+    brokerComponent: {
+      path: libDir + "integration-10-broker-component-dynamic"
+    }
+  };
+  config.components = {
+    localComponent: {
+      startMethod: "start",
+      stopMethod: "stop"
+    },
+    brokerComponent: {
+      startMethod: "start",
+      stopMethod: "stop"
+    }
+  };
+  config.cluster.ignoreBrokerPeers = ['MESH_1', 'MESH_2', 'MESH_3'];
+  return config;
+}
+
+return HappnerCluster.create(brokerInstanceConfig(commander.seq, commander.min));
---
+++ test/stress/start-brokers.js
@@ -0,0 +1,7 @@
+const commander = require('commander');
+
+commander
+  .option('--connections [number]', 'connections')
+  .option('--events [number]', 'events per connection per second')
+  .option('--methods [number]', 'methods per connection per second')
+  .option('--web [number]', 'web requests per connection per second');
