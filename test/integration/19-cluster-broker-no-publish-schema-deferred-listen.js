7643ba274ba12c36234a3e31e72cf0c20a2da461
Simon Bishop <simon@tenacious.digital>
2020-07-30 16:57:13 +0200

    test: neatened up SMC-617

--- test/integration/19-cluster-broker-no-publish-schema-deferred-listen.js
+++ test/integration/19-cluster-broker-no-publish-schema-deferred-listen.js
@@ -1,20 +1,74 @@
 const HappnerCluster = require("../..");
-var Promise = require("bluebird");
-
-
-var libDir = require("../_lib/lib-dir");
-var baseConfig = require("../_lib/base-config");
-var stopCluster = require("../_lib/stop-cluster");
-var users = require("../_lib/users");
-var testclient = require("../_lib/client");
-
-var clearMongoCollection = require("../_lib/clear-mongo-collection");
-//var log = require('why-is-node-running');
+const libDir = require("../_lib/lib-dir");
+const baseConfig = require("../_lib/base-config");
+const stopCluster = require("../_lib/stop-cluster");
+const users = require("../_lib/users");
+const testclient = require("../_lib/client");
+const clearMongoCollection = require("../_lib/clear-mongo-collection");
 const test = require("../_lib/test-helper");
+
 describe(test.testName(__filename, 3), function() {
   this.timeout(40000);
+  let servers = [];
 
-  var servers = [];
+  beforeEach("clear mongo collection", function(done) {
+    stopCluster(servers, function(e) {
+      if (e) return done(e);
+      servers = [];
+      clearMongoCollection("mongodb://localhost", "happn-cluster", function() {
+        done();
+      });
+    });
+  });
+
+  it("starts the cluster broker first, client connects and receives no further schema updates, when we flip-flop internal host", async () => {
+    let schemaPublicationCount = 0;
+    let edgeInstance = await startEdge(1, 1);
+    let internalInstance = await startInternal(2, 2);
+    await test.delay(5e3);
+    await users.add(edgeInstance, "username", "password");
+    const client = await testclient.create("username", "password", 55001);
+    await client.data.on("/mesh/schema/description", () => {
+      schemaPublicationCount++;
+    });
+    await internalInstance.stop({ reconnect: false });
+    await test.delay(5e3);
+    servers.pop(); //chuck the stopped server away
+    await startInternal(2, 2);
+    await test.delay(5e3);
+    test.expect(schemaPublicationCount).to.be(0);
+  });
+
+  afterEach("stop cluster", function(done) {
+    if (!servers) return done();
+    stopCluster(servers, function() {
+      clearMongoCollection("mongodb://localhost", "happn-cluster", function() {
+        done();
+      });
+    });
+  });
+
+  function startInternal(id, clusterMin) {
+    return new Promise((resolve, reject) => {
+      return HappnerCluster.create(remoteInstanceConfig(id, clusterMin))
+        .then(function(instance) {
+          servers.push(instance);
+          resolve(instance);
+        })
+        .catch(reject);
+    });
+  }
+
+  function startEdge(id, clusterMin, dynamic) {
+    return new Promise((resolve, reject) => {
+      return HappnerCluster.create(localInstanceConfig(id, clusterMin, dynamic))
+        .then(function(instance) {
+          servers.push(instance);
+          resolve(instance);
+        })
+        .catch(reject);
+    });
+  }
 
   function localInstanceConfig(seq, sync, dynamic) {
     var config = baseConfig(seq, sync, true);
@@ -67,65 +121,4 @@ describe(test.testName(__filename, 3), function() {
     };
     return config;
   }
-
-  beforeEach("clear mongo collection", function(done) {
-    stopCluster(servers, function(e) {
-      if (e) return done(e);
-      servers = [];
-      clearMongoCollection("mongodb://localhost", "happn-cluster", function() {
-        done();
-      });
-    });
-  });
-
-  afterEach("stop cluster", function(done) {
-    if (!servers) return done();
-    stopCluster(servers, function() {
-      clearMongoCollection("mongodb://localhost", "happn-cluster", function() {
-        done();
-      });
-    });
-  });
-
-  function startInternal(id, clusterMin) {
-    return new Promise((resolve, reject) => {
-      return HappnerCluster.create(remoteInstanceConfig(id, clusterMin))
-        .then(function(instance) {
-          servers.push(instance);
-          resolve(instance);
-        })
-        .catch(reject);
-    });
-  }
-
-  function startEdge(id, clusterMin, dynamic) {
-    return new Promise((resolve, reject) => {
-      return HappnerCluster.create(localInstanceConfig(id, clusterMin, dynamic))
-        .then(function(instance) {
-          servers.push(instance);
-          resolve(instance);
-        })
-        .catch(reject);
-    });
-  }
-
-  context("exchange", function() {
-    it("starts the cluster broker first, client connects and receives no further schema updates, when we flip-flop internal host", async () => {
-      let schemaPublicationCount = 0;
-      let edgeInstance = await startEdge(1, 1);
-      let internalInstance = await startInternal(2, 2);
-      await test.delay(5e3);
-      await users.add(edgeInstance, "username", "password");
-      const client = await testclient.create("username", "password", 55001);
-      await client.data.on("/mesh/schema/description", () => {
-        schemaPublicationCount++;
-      });
-      await internalInstance.stop({ reconnect: false });
-      await test.delay(5e3);
-      servers.pop(); //chuck the stopped server away
-      await startInternal(2, 2);
-      await test.delay(5e3);
-      test.expect(schemaPublicationCount).to.be(0);
-    });
-  });
 });
